05699cb972028cc82c756481c902edbaccca96d4
makkar-vineet <vmakkar64@gmail.com>
2024-03-07 19:23:38 +0530

    sub files are committed

--- README.md
+++
@@ -1,3 +0,0 @@
-# amazon-landingpage-clone
-<h4>First Project</h4>
-Amazon landing page clone
---
+++ script.js
@@ -0,0 +1,48 @@
+// let a ="is the answer"
+
+// let sqrt = (a,cb) =>a*a % 2 == 0 ? 'even '+cb: 'odd'+cb
+
+// function stringAns(){
+// return a;
+// }
+// console.log(sqrt(10,stringAns));
+
+ var array= [12, 13, 14]
+ let newArray=[];
+ a=10;
+ console.log("value of a =",a);
+
+//  let arrnew = array.filter(i => i%2==0)
+//  console.log(arrnew);
+
+ for(let i=0;i<array.length;i++){
+    if(array[i]%2==0){
+        newArray.push(array[i])
+
+    }
+ }
+ console.log(newArray);
+
+
+// var person =[{
+//     firstName:"vineet",
+//     lastName:"Makkar"
+// },{
+//     firstName:"vineet",
+//     lastName:"Makkar"}
+// ]
+
+
+
+// for(const a of person){
+//     console.log(a);
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     if(index == array.length-1){
+//         var temp = array[index];
+//       array[index]= array.length-1;
+//     }
+    
+// }
+// console.log(array);
